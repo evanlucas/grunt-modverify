a15014dfab1b39c4556d84998524c59b877e3c23
Evan Lucas <evanlucas@me.com>
2013-09-02 21:27:36 -0500

    Initial commit, still needs readme

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+/*
+ * grunt-modverify
+ * https://github.com/evanlucas/grunt-modverify
+ *
+ * Copyright (c) 2013 Evan Lucas
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Configuration to be run (and then tested).
+    modverify: {
+      default_options: {
+        options: {
+          fileFilter: ['*.js'],
+          directoryFilter: ['!.git', '!components', '!bower_components', '!node_modules']
+        }
+      }
+    }
+
+  });
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks')
+
+  // These plugins provide necessary tasks.
+/*   grunt.loadNpmTasks('grunt-cafe-mocha') */
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['modverify'])
+
+};
---
+++ tasks/modverify.js
@@ -0,0 +1,67 @@
+/*
+ * grunt-modverify
+ * https://github.com/evanlucas/grunt-modverify
+ *
+ * Copyright (c) 2013 Evan Lucas
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var verify = require('modverify')
+  , path = require('path')
+
+module.exports = function(grunt) {
+
+  grunt.registerMultiTask('modverify', 'Verify that your dependencies are registered in your package.json', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      punctuation: '.',
+      separator: ', ',
+      fileFilter: ['*.js'],
+      directoryFilter: ['!.git', '!components', '!bower_components', '!node_modules']
+    });
+    
+    var async = this.async()
+    
+    verify.processForDir(process.cwd(), options, function(err, output) {
+      if (err) {
+        grunt.fatal(err)
+      }
+      var pkg = require(path.join(process.cwd(), 'package.json'))
+        , deps = pkg.dependencies
+        , devDeps = pkg.devDependencies || {}
+        , modules = output.modules
+        , relativeModules = output.relativeModules
+      
+      grunt.log.write('Checking dependencies...')
+      modules.forEach(function(mod) {
+        if (deps.hasOwnProperty(mod)) {
+          grunt.verbose.writeln(mod, 'is registered as a dependency')
+        } else if (devDeps.hasOwnProperty(mod)) {
+          grunt.verbose.writeln(mod, 'is registered as a dev dependency')
+        } else {
+          grunt.log.error(mod, 'is NOT registered as a dependency')
+          grunt.fatal('Check your package.json')
+        }
+      })
+      grunt.log.ok()
+      
+      grunt.log.write('Checking relative dependencies...')
+      var keys = Object.keys(relativeModules)
+      keys.forEach(function(key) {
+        if (!verify.fileWithNameExists(key)) {
+          grunt.log.error(key, 'does not exists')
+          grunt.log.error('It is referenced from the following files:')
+          var refs = relativeModules[key]
+          refs.forEach(function(r, index) {
+            grunt.log.error(' - ', index, ' ', r)
+          })
+          grunt.fatal('Check your package.json')
+        }
+      })
+      grunt.log.ok()
+    })
+  });
+
+};
